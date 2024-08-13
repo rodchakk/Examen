fc5c2fbfc7b2469027b270821480433fc51da905
Rodolfo Chacon <rudychacon01@gmail.com>
2024-08-13 13:53:29 -0600

    avance

--- ConsoleApp1/Asignatura.cs
+++ ConsoleApp1/Asignatura.cs
@@ -5,6 +5,7 @@
 
 
 
+using Microsoft.VisualBasic;
 using System;
 using System.Security.Cryptography.X509Certificates;
 
@@ -50,40 +51,39 @@ namespace ConsoleApp1
         {
 
 
-            string msg;
+            string msg="";
 //1.De 0 % a 59 %: Reprobado
 //2.De 60 % a 79 %: Bueno
 //3.De 80 % a 89 %: Muy Bueno
 //4.De 90 % a 100 % Sobresaliente
 
-
-
             if (CalcularNotaFinal() <= 59)
-
             {
-                msg="Reprobado";
-            }
-            if (CalcularNotaFinal() <= 79)
+
+                msg = "Aprobado";
+
+            } else if(CalcularNotaFinal() <= 79)
             {
                 msg = "Bueno";
-
-            }
-
-
-            if(CalcularNotaFinal() <= 89)
+            } else if(CalcularNotaFinal() <= 89)
             {
                 msg = "Muy Bueno";
-            }
-
-            if(CalcularNotaFinal() <= 100)
+            } else if(CalcularNotaFinal() <= 100)
             {
-
                 msg = "Sobresaliente";
+            }else
+            {
+                msg = "Nota fuera de rango";
             }
 
-          
 
-            return MensajeNotaFinal();
+
+
+
+
+
+
+            return msg;
 
         }
 
@@ -101,9 +101,9 @@ namespace ConsoleApp1
             Console.WriteLine("Nombre de Clase: " + NombreAsignatura);
             Console.WriteLine("Horario: " + Horario);
             Console.WriteLine("Nombre Del Docente: " + NombreDocente);
-            Console.WriteLine("La Nota Final es: " + CalcularNotaFinal());
-            Console.WriteLine("La Nota Final es: " + CalcularNotaFinald(N1, N2, N3));
-            Console.WriteLine("Mensaje es:" + MensajeNotaFinal());
+            Console.WriteLine("La Nota Final es: " + CalcularNotaFinal() + " " + MensajeNotaFinal());
+            Console.WriteLine("La Nota Final es: " + CalcularNotaFinald(N1, N2, N3) + " " + MensajeNotaFinal());
+            
             
           
           
